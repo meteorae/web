38046f35b236a2e4beadfb9b902ecbd7295a5de5
MrTimscampi <julien.machiels@protonmail.com>
2022-02-19 22:55:17 +0100

    ci(release): fix Sentry release process

--- craco.config.ts
+++ craco.config.ts
@@ -1,7 +1,5 @@
-import { ESLINT_MODES, whenProd } from '@craco/craco';
-import SentryWebpackPlugin from '@sentry/webpack-plugin';
+import { ESLINT_MODES } from '@craco/craco';
 import path from 'path';
-import VERSION from './src/utils/version';
 
 const cracoConfig = {
   eslint: {
@@ -10,23 +8,6 @@ const cracoConfig = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-    },
-    plugins: {
-      add: [
-        ...whenProd(
-          () => [
-            new SentryWebpackPlugin({
-              authToken: process.env.SENTRY_AUTH_TOKEN,
-              org: 'meteorae',
-              project: 'react',
-              release: VERSION,
-              include: '.',
-              ignore: ['node_modules', 'webpack.config.js'],
-            }),
-          ],
-          [],
-        ),
-      ],
     },
   },
 };
