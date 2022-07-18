9db6a12db67b839c470e9c506ab88d0d05f7b965
MrTimscampi <julien.machiels@protonmail.com>
2022-07-18 18:22:02 +0200

    chore(tv): initial commit

---
+++ apps/tv/src/App.js
@@ -0,0 +1,58 @@
+import { Lightning, Utils } from '@lightningjs/sdk';
+
+export default class App extends Lightning.Component {
+  static getFonts() {
+    return [
+      { family: 'Regular', url: Utils.asset('fonts/Roboto-Regular.ttf') },
+    ];
+  }
+
+  static _template() {
+    return {
+      Background: {
+        w: 1920,
+        h: 1080,
+        color: 0xfffbb03b,
+        src: Utils.asset('images/background.png'),
+      },
+      Logo: {
+        mountX: 0.5,
+        mountY: 1,
+        x: 960,
+        y: 600,
+        src: Utils.asset('images/logo.png'),
+      },
+      Text: {
+        mount: 0.5,
+        x: 960,
+        y: 720,
+        text: {
+          text: "Let's start Building!",
+          fontFace: 'Regular',
+          fontSize: 64,
+          textColor: 0xbbffffff,
+        },
+      },
+    };
+  }
+
+  _init() {
+    this.tag('Background')
+      .animation({
+        duration: 15,
+        repeat: -1,
+        actions: [
+          {
+            t: '',
+            p: 'color',
+            v: {
+              0: { v: 0xfffbb03b },
+              0.5: { v: 0xfff46730 },
+              0.8: { v: 0xfffbb03b },
+            },
+          },
+        ],
+      })
+      .start();
+  }
+}
